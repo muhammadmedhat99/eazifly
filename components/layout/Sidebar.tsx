d10bb949609579ee81284541a0f260504e5e7f87
muahammad medhat <muhammadmedhat12@gmail.com>
2025-05-05 23:32:39 +0300

    accordion item

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -3,11 +3,21 @@
 import React from "react";
 
 import { Accordion, AccordionItem } from "@heroui/react";
-import { Profile2User } from "iconsax-reactjs";
+import { ArrowLeft2, Profile2User } from "iconsax-reactjs";
+import Image from "next/image";
+
+import Link from "next/link";
 
 export const Sidebar = () => {
   return (
     <div className="w-[200px] bg-main border-e border-stroke min-h-screen">
+      <Image
+        src="/img/logo/logo.svg"
+        alt="logo"
+        width={200}
+        height={74}
+        className="mb-4"
+      />
       <Accordion selectionMode="multiple" variant="splitted">
         <AccordionItem
           key="stydents"
@@ -20,11 +30,26 @@ export const Sidebar = () => {
           }
           classNames={{
             trigger: "justify-between px-5 py-3",
-            content: "px-6 py-2",
+            content: "px-6",
+            indicator: "duration-300 -rotate-90 data-[open=true]:rotate-90",
           }}
         >
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores,
-          repellat.
+          <div className="flex gap-2 flex-col">
+            <Link
+              href="/students"
+              className="flex items-center gap-1 duration-300 hover:ms-1"
+            >
+              <ArrowLeft2 size={12} />
+              <span className="text-sm font-semibold">بيانات الطلاب</span>
+            </Link>
+            <Link
+              href="/students"
+              className="flex items-center gap-1 duration-300 hover:ms-1"
+            >
+              <ArrowLeft2 size={12} />
+              <span className="text-sm font-semibold">اشتراكات الطلاب</span>
+            </Link>
+          </div>
         </AccordionItem>
       </Accordion>
     </div>
