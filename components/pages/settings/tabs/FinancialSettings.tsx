7ad7365ba974bd617ccedf2c34a228550c619b67
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-06-18 17:15:37 +0300

    fetch payment methods data in Financial Settings page

--- components/pages/settings/tabs/FinancialSettings.tsx
+++ components/pages/settings/tabs/FinancialSettings.tsx
@@ -1,9 +1,21 @@
 import { Button, Input } from "@heroui/react";
 import React, { useState } from "react";
 import Image from "next/image";
-import { Edit2 } from "iconsax-reactjs";
+import { Edit2, EmptyWalletChange } from "iconsax-reactjs";
 import PaymentModal from "../PaymentModal";
+import { fetchClient } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { axios_config } from "@/lib/const";
+import { Loader } from "@/components/global/Loader";
 
+interface PaymentMethod {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  how_to_use: string;
+  how_to_use_type: string;
+}
 
 export const FinancialSettings = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -11,6 +23,11 @@ export const FinancialSettings = () => {
   const handleOpenModal = () => {
     setModalOpen(true);
   };
+
+  const { data: paymentData, isLoading } = useQuery({
+    queryFn: async () => await fetchClient(`client/payment/method`, axios_config),
+    queryKey: ["GetPaymentMethods"],
+  });
 
   return (
     <div className="grid grid-cols-1 gap-3 px-4 py-6">
@@ -26,28 +43,42 @@ export const FinancialSettings = () => {
           اضافة وسيلة دفع جديدة
         </Button>
       </div>
-      <div className="flex justify-between relative touch-none tap-highlight-transparent select-none w-full bg-content1 items-center border border-stroke max-w-full justify-between rounded-2xl gap-2 p-4 data-[selected=true]:border-success">
-        <div className="flex items-center gap-2">
-          <Image
-            src="/img/static/instapay.png"
-            width={32}
-            height={32}
-            alt="instapay"
-          />
-          <div className="flex flex-col gap-1">
-            <p className="font-bold text-black-text">تطبيق إنستا باي</p>
-            <p className="font-bold text-xs text-title/60">
-              مثال :هذا النص هو جزء من عملية تحسين تجربة المستخدم من خلال
-              النص. مثال :هذا النص هو جزء من عملية تحسين{" "}
-            </p>
-          </div>
-        </div>
-        <Button onPress={handleOpenModal} className="flex items-center gap-1 text-primary bg-transparent hover:bg-transparent active:bg-transparent shadow-none border-none">
-          <Edit2 size={18} />
 
-          <span className="text-sm font-bold">تعديل</span>
-        </Button>
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        paymentData?.data?.map((item: PaymentMethod) => (
+          <div
+            key={item.id}
+            className="flex justify-between relative touch-none tap-highlight-transparent select-none w-full bg-content1 items-center border border-stroke max-w-full justify-between rounded-2xl gap-2 p-4 data-[selected=true]:border-success"
+          >
+            <div className="flex items-center gap-2">
+              {item.image ? (
+                <Image
+                  src={item.image}
+                  width={32}
+                  height={32}
+                  alt="instapay"
+                />
+              ) : <EmptyWalletChange size={32} />}
+              <div className="flex flex-col gap-1">
+                <p className="font-bold text-black-text">{item.title}</p>
+                <p className="font-bold text-xs text-title/60">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+            <Button
+              onPress={handleOpenModal}
+              className="flex items-center gap-1 text-primary bg-transparent hover:bg-transparent active:bg-transparent shadow-none border-none"
+            >
+              <Edit2 size={18} />
+              <span className="text-sm font-bold">تعديل</span>
+            </Button>
+          </div>
+        ))
+      )}
+
     </div>
   );
 };
