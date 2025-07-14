59b0fa8b0393d5994f0f7c1cece5d4e29f98eff8
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-07-14 14:37:13 +0300

    display program data

--- components/pages/programs/details/tabs/ProgramContent.tsx
+++ components/pages/programs/details/tabs/ProgramContent.tsx
@@ -1,30 +1,45 @@
+import Image from "next/image";
 import React from "react";
 
-export const ProgramContent = () => {
+type MainInformationProps = {
+  mainData: {
+    id: number;
+    title: string;
+    label: string;
+    specialization: string;
+    category: string;
+    content: string;
+    description: string;
+    image: string;
+    duration: string;
+    goals: string;
+    number_of_students: number;
+    number_of_lessons: number;
+    payment_methods: {
+      id: number;
+      title: string;
+    }[];
+  };
+};
+
+export const ProgramContent = ({ mainData }: MainInformationProps) => {
   return (
     <div className="bg-main">
       {/* main info  */}
       <div className="flex flex-col md:flex-row gap-4 p-4">
-        <div className="w-full relative">
+        <div className="flex flex-col">
           <h3 className="text-primary mb-2 font-bold">صورة البرنامج</h3>
-          <div className="overflow-hidden rounded-lg h-40">
-            <img
-              src="https://placehold.co/800x400"
-              alt="Program"
-              className="w-full h-full object-cover"
+          <div>
+            <Image
+              src={mainData.image}
+              width={350}
+              height={350}
+              alt="login image"
+              className="w-full h-full flex-1 object-cover"
             />
           </div>
         </div>
-        <div className="w-full relative">
-          <h3 className="text-primary mb-2 font-bold">صورة الفائدة</h3>
-          <div className="overflow-hidden rounded-lg h-40">
-            <img
-              src="https://placehold.co/800x400"
-              alt="Audience"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
+        
       </div>
 
       {/* descriptiuon  */}
@@ -32,121 +47,26 @@ export const ProgramContent = () => {
         <h2 className="text-right text-primary font-bold mb-4">وصف البرنامج</h2>
 
         <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            1 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
           <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
-          </p>
-        </div>
-
-        <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            2 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
-          <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
+            {mainData.description}
           </p>
         </div>
       </div>
 
-      {/* What Students Will Learn Section */}
+      {/* Program goals Section */}
       <div className="p-4 ">
         <h2 className="text-right text-primary font-bold mb-4">
-          ماذا سوف يتعلم الطلاب في الدورة ؟
+          أهداف البرنامج
         </h2>
 
         <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            1 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
           <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
+            {mainData.goals}
           </p>
         </div>
 
-        <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            2 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
-          <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
-          </p>
-        </div>
       </div>
 
-      {/* Program Benefits Section */}
-      <div className="p-4 ">
-        <h2 className="text-right text-primary font-bold mb-4">
-          مزايا البرنامج
-        </h2>
-
-        <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            1 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
-          <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
-          </p>
-        </div>
-
-        <div className="mb-6">
-          <h3 className="font-semibold text-black-text mb-2">
-            2 - هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص
-          </h3>
-          <p className="text-title text-sm">
-            هذا النص هو جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو
-            جزء من عملية تحسين تجربة المستخدم خلال النص. هذا النص هو جزء من
-            عملية تحسين تجربة المستخدم خلال النص.
-          </p>
-        </div>
-      </div>
-
-      {/* Instructor Profiles Section */}
-      <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="text-center">
-          <h3 className="text-primary font-bold mb-2">إسم للف</h3>
-          <div className="overflow-hidden rounded-lg h-40">
-            <img
-              src="https://placehold.co/400x400"
-              alt="Instructor 1"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-
-        <div className="text-center">
-          <h3 className="text-primary font-bold mb-2">إسم للف</h3>
-          <div className="overflow-hidden rounded-lg h-40">
-            <img
-              src="https://placehold.co/400x400"
-              alt="Instructor 2"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-
-        <div className="text-center">
-          <h3 className="text-primary font-bold mb-2">إسم للف</h3>
-          <div className="overflow-hidden rounded-lg h-40">
-            <img
-              src="https://placehold.co/400x400"
-              alt="Instructor 3"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
