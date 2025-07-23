672998db1cf15a53f8d083a8b676e04b613019f8
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-07-23 17:28:24 +0300

    student program updates

--- components/pages/students/student-details/tabs/ProgramTabs/AddSubaccountModal.tsx
+++ components/pages/students/student-details/tabs/ProgramTabs/AddSubaccountModal.tsx
@@ -219,7 +219,7 @@ export default function AddSubaccountModal({
         onClose()
       } else {
         addToast({
-          title: "error",
+          title: response.message,
           color: "danger",
         });
       }
