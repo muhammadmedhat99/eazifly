fbc437b16cd065f8d306b94a6075d1ebd3962629
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-07-28 17:57:00 +0300

    add whatsapp country code

--- components/pages/students/student-details/AddStudentModal/index.tsx
+++ components/pages/students/student-details/AddStudentModal/index.tsx
@@ -52,6 +52,7 @@ const schema = yup
       .required("ادخل بريد إلكتروني"),
     phone: yup.string().required("ادخل رقم الهاتف"),
     country_code: yup.string().required("ادخل كود الدولة"),
+    whats_app_country_code: yup.string().required("ادخل كود الدولة"),
     whats_app: yup.string().required("ادخل رقم الواتس آب"),
     password: yup.string().required("ادخل كلمة المرور"),
     password_confirmation: yup
@@ -307,6 +308,59 @@ export default function AddStudentModal({
                                       inputWrapper: "shadow-none",
                                       base: "mb-4",
                                   }}
+                                  endContent={
+                                    data?.data?.length > 0 && (
+                                      <Controller
+                                        name="whats_app_country_code"
+                                        control={control}
+                                        defaultValue=""
+                                        render={({ field }) => (
+                                          <Select
+                                            {...field}
+                                            selectedKeys={field.value ? [field.value] : []}
+                                            onSelectionChange={(keys) => {
+                                              const selectedKey = Array.from(keys)[0];
+                                              field.onChange(selectedKey);
+                                            }}
+                                            className="max-w-40 -me-3 bg-white"
+                                            isInvalid={!!errors.whats_app_country_code?.message}
+                                            errorMessage={errors.whats_app_country_code?.message}
+                                            classNames={{
+                                              trigger: "rounded-s-none border-1 shadow-none",
+                                              listbox: "w-full",
+                                              listboxWrapper: "w-full",
+                                            }}
+                                            variant="bordered"
+                                            renderValue={(items) =>
+                                              items?.map((item: any) => (
+                                                <div key={item.key} className="flex items-center gap-2">
+                                                  {item.props.startContent}
+                                                  <span>{item.props.children}</span>
+                                                </div>
+                                              ))
+                                            }
+                                          >
+                                            {data?.data?.map((country: any) => (
+                                              <SelectItem
+                                                key={country.phone_code}
+                                                startContent={
+                                                  <Avatar
+                                                    className="w-6 h-4"
+                                                    radius="none"
+                                                    src={country?.image}
+                                                    alt="country flag"
+                                                  />
+                                                }
+                                                className="w-full"
+                                              >
+                                                {country.phone_code}
+                                              </SelectItem>
+                                            ))}
+                                          </Select>
+                                        )}
+                                      />
+                                    )
+                                  }
                               />
                               <Input
                                   label="كلمة المرور"
