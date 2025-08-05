63d4666e0145bc3418c2b5f248eca5c5e088d3fd
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-05 12:22:08 +0300

    Specialization crud

---
+++ app/[locale]/(dashboard)/settings/specializations/create/page.tsx
@@ -0,0 +1,11 @@
+
+import { CreateSpecializations } from "@/components/pages/settings/specializations/create";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <CreateSpecializations />
+    </div>
+  );
+}
