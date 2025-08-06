e2867f46023efbd14e18a6dea33c4cbf1668d38e
muahammad medhat <muhammadmedhat12@gmail.com>
2025-08-06 16:16:51 +0300

    fix/build issues

--- app/[locale]/(dashboard)/programs/update/[id]/page.tsx
+++ app/[locale]/(dashboard)/programs/update/[id]/page.tsx
@@ -5,14 +5,12 @@ import { CreateProgram } from "@/components/pages/programs/create";
 export default async function UpdateProgramPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-    const { id } = await params;
-    const cookieStore = await cookies();
-    const token = cookieStore.get("token");
-    const data = await fetchData(`client/program/show/${id}`, token?.value);
+  const { id } = await params;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token");
+  const data = await fetchData(`client/program/show/${id}`, token?.value);
 
-  return (
-    <CreateProgram initialData={data} mode="edit" />
-  );
+  return <CreateProgram initialData={data} mode="edit" />;
 }
