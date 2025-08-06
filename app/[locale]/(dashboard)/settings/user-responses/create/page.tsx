dce681dbab8b1c776b87261eba21e7245393dc96
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-06 18:35:09 +0300

    user responses crud

---
+++ app/[locale]/(dashboard)/settings/user-responses/create/page.tsx
@@ -0,0 +1,10 @@
+import { CreateResonses } from "@/components/pages/settings/user-responses/create";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <CreateResonses />
+    </div>
+  );
+}
