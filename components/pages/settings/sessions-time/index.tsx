29f326870a3abb292e2aa122332ea2b0e81792fa
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-07 11:17:28 +0300

    sessions-time crud

---
+++ components/pages/settings/sessions-time/index.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import React, { useMemo, useState } from "react";
+import TableComponent from "@/components/global/Table";
+import { Options } from "@/components/global/Icons";
+import {
+  avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@heroui/react";
+import { useDebounce } from "@/lib/hooks/useDebounce";
+import { ArrowDown2, SearchNormal1 } from "iconsax-reactjs";
+import { CustomPagination } from "@/components/global/Pagination";
+import { useQuery } from "@tanstack/react-query";
+import { fetchClient } from "@/lib/utils";
+import { axios_config } from "@/lib/const";
+import { AllQueryKeys } from "@/keys";
+import { Loader } from "@/components/global/Loader";
+import { formatDate } from "@/lib/helper";
+
+const columns = [
+  { name: "#", uid: "index" },
+  { name: "العنوان", uid: "title" },
+  { name: "مدة المحاضرة", uid: "time" },
+];
+
+const OptionsComponent = ({ id }: { id: number }) => {
+  return (
+    <Dropdown classNames={{ base: "max-w-40", content: "min-w-36" }}>
+      <DropdownTrigger>
+        <button>
+          <Options />
+        </button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem href={`/teachers/${id}`} key="show">
+          عرض البيانات
+        </DropdownItem>
+        <DropdownItem key="actions">الإجراءات</DropdownItem>
+        <DropdownItem key="delete">حذف</DropdownItem>
+        <DropdownItem key="add-to-course">تعيين علي برنامج</DropdownItem>
+        <DropdownItem key="send-mail">إرسال رسالة</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export const AllSessionsTime = () => {
+  const [nameSearch, setNameSearch] = useState("");
+  const debouncedNameSearch = useDebounce(nameSearch, 500);
+  const [selectedStatus, setSelectedStatus] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const params: Record<string, string | number> = {
+    page: currentPage,
+  };
+
+  if (debouncedNameSearch) {
+    params.name = debouncedNameSearch;
+  }
+
+  const { data: sessionsTimeData, isLoading } = useQuery({
+    queryFn: async () =>
+      await fetchClient(`client/plan/session/time`, {
+        ...axios_config,
+        params,
+      }),
+    queryKey: AllQueryKeys.GetAllSpecializations
+  });
+
+  const formattedData =
+    sessionsTimeData?.data?.map((item: any) => ({
+      id: item.id,
+      index: item.id,
+      time: `${item.time} دقيقة`,
+      title: item.title || "N/A",
+    })) || [];
+
+  return (
+    <>
+      <div className="p-4 flex items-center justify-between flex-wrap gap-4">
+        <div className="flex items-center gap-2">
+          <div className="relative md:min-w-80">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+              <SearchNormal1 size="18" className="text-gray-400" variant="Outline" />
+            </div>
+            <input
+              type="text"
+              placeholder="بحث..."
+              className="w-full py-2 h-11 ps-10 pe-4 text-sm text-right border border-stroke rounded-lg focus:outline-none focus:ring-1 focus:ring-stroke bg-light"
+              value={nameSearch}
+              onChange={(e) => setNameSearch(e.target.value)}
+            />
+          </div>
+
+          <Dropdown classNames={{ content: "min-w-36" }} showArrow>
+            <DropdownTrigger>
+              <Button
+                variant="flat"
+                color="primary"
+                className="text-primary font-semibold gap-1"
+                radius="sm"
+              >
+                <ArrowDown2 size={14} />
+                ترتيب حسب
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              <DropdownItem key="show">الإسم</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+
+        <div className="flex gap-2">
+          <Button
+            id="all"
+            variant="flat"
+            color={selectedStatus === "all" ? "primary" : "default"}
+            className="font-semibold"
+            onPress={(e) => setSelectedStatus(e.target.id)}
+          >
+            الكل
+          </Button>
+          <Button
+            id="active"
+            variant="flat"
+            color={selectedStatus === "active" ? "primary" : "default"}
+            className="font-semibold"
+            onPress={(e) => setSelectedStatus(e.target.id)}
+          >
+            نشط
+          </Button>
+          <Button
+            id="new"
+            variant="flat"
+            color={selectedStatus === "new" ? "primary" : "default"}
+            className="font-semibold"
+            onPress={(e) => setSelectedStatus(e.target.id)}
+          >
+            جديد
+          </Button>
+        </div>
+      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <TableComponent
+          columns={columns}
+          data={formattedData}
+          ActionsComponent={OptionsComponent}
+        />
+      )}
+
+      <div className="my-10 px-6">
+        <CustomPagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          last_page={sessionsTimeData?.meta?.last_page}
+          total={sessionsTimeData?.meta?.total}
+        />
+      </div>
+    </>
+  );
+};
