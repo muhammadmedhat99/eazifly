e42e2deb526a9d12d5b3a3b17787209c501de19e
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-10 17:51:19 +0300

    report questions crud

---
+++ app/[locale]/(dashboard)/settings/report-questions/create/page.tsx
@@ -0,0 +1,10 @@
+import { CreateQuestions } from "@/components/pages/settings/report-questions/create";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <CreateQuestions />
+    </div>
+  );
+}
