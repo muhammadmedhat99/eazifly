ab90da64ffd812b5c0e07dab96f19180e1a12f6e
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-10 14:13:28 +0300

    Coupons crud

---
+++ app/[locale]/(dashboard)/settings/coupons/create/page.tsx
@@ -0,0 +1,11 @@
+
+import { CreateCoupons } from "@/components/pages/settings/coupons/create";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <CreateCoupons />
+    </div>
+  );
+}
