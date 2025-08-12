81c9149169d7b67c33f9d7cf28ad9716dd89d810
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-08-12 16:35:55 +0300

    specialization update

--- components/pages/programs/details/tabs/MainInformation.tsx
+++ components/pages/programs/details/tabs/MainInformation.tsx
@@ -18,6 +18,7 @@ import { Loader } from "@/components/global/Loader";
 import { getCookie } from "cookies-next";
 import { postData } from "@/lib/utils";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import SpecializationUpdateModal from "./SpecializationUpdateModal";
 
 type MainInformationProps = {
   data: {
@@ -53,6 +54,7 @@ type MainInformationProps = {
 export const MainInformation = ({ data, refetch }: MainInformationProps) => {
   const queryClient = useQueryClient();
   const [modalOpen, setModalOpen] = useState(false);
+  const [specializationmodalOpen, setSpecializationModalOpen] = useState(false);
   const handleCopy = async (textToCopy: string) => {
     try {
       await navigator.clipboard.writeText(textToCopy);
@@ -102,127 +104,141 @@ export const MainInformation = ({ data, refetch }: MainInformationProps) => {
       });
     },
   });
-  
+
   return (
     <>
-    {Object.keys(data || {}).length > 0 ? (
-      <div className="p-5 grid grid-cols-1 lg:grid-cols-2 gap-4">
-      <PaymentMethodsUpdateModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-        initialData={data}
-      /> 
-      {/* Main Card  */}
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
-        <div className="text-primary font-bold">إسم البرنامج</div>
-        <div className="text-black-text font-bold text-[15px]">
-          {data.title}
-        </div>
-      </div>
-      {/* Main Card  */}
-      {/* Main Card  */}
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex justify-between items-center">
-        <div className="flex flex-col gap-2">
-          <div className="text-primary font-bold">الحالة</div>
-          <div className="w-fit">
-            <div
-              className={`text-${data?.status?.color}
-                bg-${data?.status?.color} bg-opacity-10
-                px-5 py-1 rounded-3xl font-bold text-[15px]`}
-            >
-              {data?.status?.label}
+      {Object.keys(data || {}).length > 0 ? (
+        <div className="p-5 grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <PaymentMethodsUpdateModal
+            isOpen={modalOpen}
+            onClose={() => setModalOpen(false)}
+            initialData={data}
+          />
+          <SpecializationUpdateModal
+            isOpen={specializationmodalOpen}
+            onClose={() => setSpecializationModalOpen(false)}
+            initialData={data}
+          />
+          {/* Main Card  */}
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
+            <div className="text-primary font-bold">إسم البرنامج</div>
+            <div className="text-black-text font-bold text-[15px]">
+              {data.title}
             </div>
           </div>
-        </div>
-
-        {data?.status?.key === "uncompleted" && (
-          <button
-            type="button"
-            className="flex items-center gap-1 text-sm font-bold text-success"
-            onClick={() => handleStatusChange("published")}
-          >
-            <Edit2 size={18} />
-            نشر
-          </button>
-        )}
-
-        {data?.status?.key === "published" && (
-          <button
-            type="button"
-            className="flex items-center gap-1 text-sm font-bold text-warning"
-            onClick={() => handleStatusChange("archived")}
-          >
-            <Edit2 size={18} />
-            أرشفة
-          </button>
-        )}
-
-        {data?.status?.key === "archived" && (
-          <button
-            type="button"
-            className="flex items-center gap-1 text-sm font-bold text-success"
-            onClick={() => handleStatusChange("published")}
-          >
-            <Edit2 size={18} />
-            نشر
-          </button>
-        )}
-      </div>
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
-        <div className="text-primary font-bold">عنوان البرنامج</div>
-        <div className="text-black-text font-bold text-[15px]">
-          {data.label}
-        </div>
-      </div>
-      {/* Main Card  */}
-
-      {/* Main Card  */}
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
-        <div className="text-primary font-bold">التخصص</div>
-        <div className="flex items-center gap-3 flex-wrap">
-          <div className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm">
-            {data.specialization}
-          </div>
-        </div>
-      </div>
-      {/* Main Card  */}
-      {/* Main Card  */}
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex items-center justify-between">
-        <div className="flex flex-col gap-2">
-          <div className="text-primary font-bold">وسائل الدفع</div>
-          <div className="flex items-center gap-3 flex-wrap">
-            {data.payment_methods.map((method) => (
-              <div
-                className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm"
-                key={method.id}
-              >
-                {method.title}
+          {/* Main Card  */}
+          {/* Main Card  */}
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex justify-between items-center">
+            <div className="flex flex-col gap-2">
+              <div className="text-primary font-bold">الحالة</div>
+              <div className="w-fit">
+                <div
+                  className={`text-${data?.status?.color}
+                bg-${data?.status?.color} bg-opacity-10
+                px-5 py-1 rounded-3xl font-bold text-[15px]`}
+                >
+                  {data?.status?.label}
+                </div>
               </div>
-            ))}
-          </div>
-        </div>
-        <Button
-          onPress={()=>setModalOpen(true)}
-          className="flex items-center gap-1 text-primary bg-transparent hover:bg-transparent active:bg-transparent shadow-none border-none"
-        >
-          <Edit2 size={18} />
-          <span className="text-sm font-bold">تعديل</span>
-        </Button>
-      </div>
-      {/* Main Card  */}
-       {/* Main Card  */}
-      <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
-        <div className="text-primary font-bold">الاستضافة</div>
-        {data?.host && <div
-          className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm w-fit"
-          key={data?.host?.id}
-        >
-          {data?.host?.title}
-        </div>}
-      </div>
-      {/* Main Card  */}
+            </div>
 
-      {/* <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2 lg:col-span-2">
+            {data?.status?.key === "uncompleted" && (
+              <button
+                type="button"
+                className="flex items-center gap-1 text-sm font-bold text-success"
+                onClick={() => handleStatusChange("published")}
+              >
+                <Edit2 size={18} />
+                نشر
+              </button>
+            )}
+
+            {data?.status?.key === "published" && (
+              <button
+                type="button"
+                className="flex items-center gap-1 text-sm font-bold text-warning"
+                onClick={() => handleStatusChange("archived")}
+              >
+                <Edit2 size={18} />
+                أرشفة
+              </button>
+            )}
+
+            {data?.status?.key === "archived" && (
+              <button
+                type="button"
+                className="flex items-center gap-1 text-sm font-bold text-success"
+                onClick={() => handleStatusChange("published")}
+              >
+                <Edit2 size={18} />
+                نشر
+              </button>
+            )}
+          </div>
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
+            <div className="text-primary font-bold">عنوان البرنامج</div>
+            <div className="text-black-text font-bold text-[15px]">
+              {data.label}
+            </div>
+          </div>
+          {/* Main Card  */}
+
+          {/* Main Card  */}
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex items-center justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-primary font-bold">التخصص</div>
+              <div className="flex items-center gap-3 flex-wrap">
+                <div className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm">
+                  {data.specialization}
+                </div>
+              </div>
+            </div>
+            <Button
+              onPress={() => setSpecializationModalOpen(true)}
+              className="flex items-center gap-1 text-primary bg-transparent hover:bg-transparent active:bg-transparent shadow-none border-none"
+            >
+              <Edit2 size={18} />
+              <span className="text-sm font-bold">تعديل</span>
+            </Button>
+          </div>
+          {/* Main Card  */}
+          {/* Main Card  */}
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex items-center justify-between">
+            <div className="flex flex-col gap-2">
+              <div className="text-primary font-bold">وسائل الدفع</div>
+              <div className="flex items-center gap-3 flex-wrap">
+                {data.payment_methods.map((method) => (
+                  <div
+                    className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm"
+                    key={method.id}
+                  >
+                    {method.title}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <Button
+              onPress={() => setModalOpen(true)}
+              className="flex items-center gap-1 text-primary bg-transparent hover:bg-transparent active:bg-transparent shadow-none border-none"
+            >
+              <Edit2 size={18} />
+              <span className="text-sm font-bold">تعديل</span>
+            </Button>
+          </div>
+          {/* Main Card  */}
+          {/* Main Card  */}
+          <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2">
+            <div className="text-primary font-bold">الاستضافة</div>
+            {data?.host && <div
+              className="bg-primary/10 py-2 px-4 rounded-xl text-primary font-bold text-sm w-fit"
+              key={data?.host?.id}
+            >
+              {data?.host?.title}
+            </div>}
+          </div>
+          {/* Main Card  */}
+
+          {/* <div className="bg-white border border-stroke rounded-xl px-5 py-6 flex flex-col gap-2 lg:col-span-2">
         <div className="text-primary font-bold">انواع الدفع</div>
 
         <Table
@@ -269,8 +285,8 @@ export const MainInformation = ({ data, refetch }: MainInformationProps) => {
           </TableBody>
         </Table>
       </div> */}
-    </div>
-    ) : <Loader/>}
+        </div>
+      ) : <Loader />}
     </>
   );
 };
