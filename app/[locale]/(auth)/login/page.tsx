aea6c73e591fbccd6db0124ae61b3b82cf8364d2
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-09-02 15:54:41 +0300

    handle login page responsive and phone code

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -6,21 +6,23 @@ import { LoginForm } from "@/components/pages/login/form";
 
 export default function App() {
   return (
-    <div className="bg-[#E9EFF7] dark:bg-[#0b112c] flex items-center justify-between w-screen h-screen">
-      <div className="flex flex-col gap-2 flex-1 px-14">
-        <p className="text-xl font-bold">اهلا بعودتك مجددا ..!</p>
-        <p className="text-[#5E5E5E] text-sm font-bold mb-10">
+    <div className="bg-[#E9EFF7] dark:bg-[#0b112c] flex flex-col md:flex-row items-center justify-between w-screen h-screen">
+      <div className="flex flex-col gap-2 flex-1 px-6 md:px-14 py-8 md:py-0">
+        <p className="text-xl md:text-2xl font-bold">اهلا بعودتك مجددا ..!</p>
+        <p className="text-[#5E5E5E] text-sm md:text-base font-bold mb-6 md:mb-10">
           سجل الدخول إلي حسابك للمتابعه في منصتنا التعليميه
         </p>
         <LoginForm />
       </div>
+
       <Image
         src={myImage}
         width={1024}
         height={1024}
         alt="login image"
-        className="w-1/2 h-full flex-1 object-cover"
+        className="hidden md:block w-1/2 h-full object-cover"
       />
     </div>
   );
 }
+
