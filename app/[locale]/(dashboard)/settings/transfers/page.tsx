faa6b5d9ef9d3bdbdaa1ed6da444a3677b4d1a7c
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-09-18 00:02:11 +0300

    add transfers in settings

---
+++ app/[locale]/(dashboard)/settings/transfers/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+
+import { BreadCrumb } from "@/components/global/BreadCrumb";
+import { AllTransfers } from "@/components/pages/settings/transfers";
+
+const BreadCrumbItems = [
+  {
+    id: 1,
+    name: "الرئيسية",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "الإعدادات",
+    link: "/settings/general"
+  },
+  {
+    id: 3,
+    name: "إعدادات طرق التحويل",
+  },
+];
+
+export default function page() {
+  return (
+    <div>
+      <BreadCrumb items={BreadCrumbItems}>
+      <div className="flex flex-wrap items-center gap-2">
+          <Link
+            href="/settings/transfers/create"
+            className="text-white font-semibold text-xs sm:text-sm px-4 sm:px-6 py-1.5 sm:py-2 rounded-md bg-primary"
+          >
+            أضافة طريقة تحويل 
+          </Link>
+        </div>
+      </BreadCrumb>
+
+      <AllTransfers />
+    </div>
+  );
+}
