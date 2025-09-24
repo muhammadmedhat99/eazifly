0680e81fb9be45731b95423802a0659724efd356
ruaaeldeeb <ruaa.reda99@gmail.com>
2025-09-24 17:40:20 +0300

    fix children users fetch in students

--- components/pages/students/student-details/tabs/Programs.tsx
+++ components/pages/students/student-details/tabs/Programs.tsx
@@ -371,7 +371,7 @@ export const Programs = ({
 
   const instructorsResults = useQueries({
     queries: subscriptionsData.data.map((subscription: Subscription) => ({
-      queryKey: ["instructors", subscription.id],
+      queryKey: ["instructors", subscription.main_subscription_id],
       queryFn: async () =>
         await fetchClient("client/program/instructors", {
           ...axios_config,
@@ -401,7 +401,7 @@ export const Programs = ({
 
   const subaccountsResults = useQueries({
     queries: subscriptionsData.data.map((subscription: Subscription) => ({
-      queryKey: ["programsubaccounts", subscription.id],
+      queryKey: ["programsubaccounts", subscription.main_subscription_id],
       queryFn: async () =>
         await fetchClient(`client/children/users/${user_id}`, {
           ...axios_config,
